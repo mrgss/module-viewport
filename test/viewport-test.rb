f2a0ae9114dcfb5f89174d70e48125723dec5b1c
Manuel Quinteros <quinterosmanu@mail.com>
2015-10-04 16:35:34 -0300

    simple test

--- test/viewport-test.rb
+++ test/viewport-test.rb
@@ -5,3 +5,8 @@
 #assert('C and Ruby Extension Example 2') do
 #  CRubyExtension.respond_to? :ruby_method
 #end
+
+assert("Viewport initialize") do
+	viewp = MRGSS::Viewport.new(MRGSS::Rect.new(0,0,25,25))
+    viewp.nil?
+end
